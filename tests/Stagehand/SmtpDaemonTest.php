cacdd1166619cd621ebd799bd4437f8b48324b5c
mbarracuda <mbarracuda@gmail.com>
2009-11-10 00:04:24 +0900

    Added a smtp daemon.

---
+++ tests/Stagehand/SmtpDaemonTest.php
@@ -0,0 +1,148 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP version 5
+ *
+ * Copyright (c) 2009 mbarracuda <mbarracuda@gmail.com>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Stagehand_SmtpDaemon
+ * @copyright  2009 mbarracuda <mbarracuda@gmail.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @since      File available since Release 0.1.0
+ */
+
+// {{{ Stagehand_PHP_SmtpDaemonTest
+
+/**
+ * Some tests for Stagehand_SmtpDaemon
+ *
+ * @package    Stagehand_SmtpDaemon
+ * @copyright  2009 mbarracuda <mbarracuda@gmail.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @since      Class available since Release 0.1.0
+ */
+class Stagehand_SmtpDaemonTest extends PHPUnit_Framework_TestCase
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access protected
+     */
+
+    protected $socket;
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    public function setUp()
+    {
+        $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+        socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO,
+                          array('sec' => 1, 'usec' => 0)
+                          );
+    }
+
+    public function tearDown()
+    {
+        socket_close($this->socket);
+    }
+
+    /**
+     * @test
+     */
+    public function connect()
+    {
+        $this->assertTrue(@socket_connect($this->socket, 'localhost', 9025));
+
+        $result = null;
+        if (!socket_recv($this->socket, $result, 2048, 0)) {
+            $this->fail('timeout');
+        }
+
+        $this->assertEquals($result, "220\r\n");
+    }
+
+    /**
+     * @test
+     */
+    public function commandHelo()
+    {
+        $this->assertTrue(@socket_connect($this->socket, 'localhost', 9025));
+        socket_recv($this->socket, $result, 2048, 0);
+
+        $data = "HELO localhost\r\n";
+        socket_write($this->socket, $data, strlen($data));
+
+        $result = null;
+        if (!socket_recv($this->socket, $result, 2048, 0)) {
+            $this->fail('timeout');
+        }
+
+        $this->assertEquals($result, "250\r\n");
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
